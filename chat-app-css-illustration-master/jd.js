e83331c19c593e42e1f5702222f6c0bfc3f32504
AmrMagdyElmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2020-08-11 15:06:52 +0200

    Chat illustration
    Using Html & Css & JavaScript

---
+++ chat-app-css-illustration-master/jd.js
@@ -0,0 +1,5 @@
+document.querySelector(".Mess2").classList.add("Messages");
+document.querySelector(".Mess3").classList.add("Messages");
+document.querySelector(".Mess5").classList.add("Messages");  
+document.querySelector(".Mess6").classList.add("Messages");   
+document.querySelector(".Mess7").classList.add("Messages");  
